d804f9f7ffcef48e21aaf7d9463bffdd5e4ba45c
checkmarx-sample <38136346+checkmarx-sample@users.noreply.github.com>
2019-08-07 16:54:02 -0400

    Update QuickTest.java
    commit

--- QuickTest.java
+++ QuickTest.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
+//---------------
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
